efa3dd159bca6bc280b47315bb995e40b06e5bde
Kristian Kelij <k13.kelij@gmail.com>
2023-03-19 15:17:02 +0100

    dotenv problem solved, working solution

--- src/main/java/dev/kelij/movies/MovieController.java
+++ src/main/java/dev/kelij/movies/MovieController.java
@@ -1,5 +1,6 @@
 package dev.kelij.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> getAllMovies(){
-        return new ResponseEntity<String>("Aloha picula!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
 }
